bb63c78d5a83ab10607832d1fe37bf0db814b1d5
weidazhang <weidazhang@physics.ox.ac.uk>
2025-07-25 15:58:21 +0100

    detailed bin->hex->str format converstions to adapt PamirSerial base class

--- flash_load.py
+++ flash_load.py
@@ -2,35 +2,45 @@ import time
 import os
 import sys
 import serial
+import logging
 from typing import Literal
 
 from serial_commport.Pamir_serial_basic import PamirSerial
+
 # It turns out "flash_erase", "flash_write", "flash_read" has been provided by [Meenu S]
 # You would want to override them and potentially encapsulate them
-FLASH_PAGE_SIZE         = 256          # bytes  per program-page
-FLASH_SECTOR_SIZE       = 0x10000      # 64 KiB per sector
-FLASH_BASE_GOLDEN_BYTES = 0x00000000
-MAX_SIZE_BYTES = 0x00F50000
-FLASH_MAX_GOLDEN_BYTES  = 0x00F50000
-FLASH_BASE_OPERATION_BYTES = 0x01000000
-FLASH_MAX_OPERATION_BYTES = 0x01F50000
-FLASH_WIP_TIMEOUT_SEC   = 10.0
+FLASH_PAGE_SIZE = 256  # bytes  per program-page
+FLASH_SECTOR_SIZE = 0x10000  # 64 KiB per sector
+FLASH_ADDRBASE_GOLDEN = 0x00000000
+FLASH_ADDRBASE_OPERATION = 0x01000000
+IMAGE_MAX_SIZE_BYTES = FLASH_ADDRBASE_OPERATION
+FLASH_WIP_TIMEOUT_SEC = 10.0
 FLASH_POLL_INTERVAL_SEC = 0.05
-EXPECTED_FLASH_ID = "20bb19"
+FPGA_BITSTREAM_SYNC_WORD = (
+    "AA995566"  # 4 bytes sync word at the start of the bitstream file
+)
+FPGA_BITSTREAM_IDCODE = {
+    "XCKU040": "3822093"  # Expected IDCODE for the FPGA
+}
+ALLOW_EXTS = [".bin"]
+
 
 class FlashLoad(PamirSerial):
     @staticmethod
     def read_binary_to_hex(filename, max_bytes=FLASH_PAGE_SIZE) -> dict:
         return_dict = {"status": True, "msg": None}
-        hex_pages = []
+        hex_pages: list[str] = []
+        bin_pages: list[bytes] = []
         try:
-            with open(filename, 'rb') as f:
+            with open(filename, "rb") as f:
                 while True:
                     data = f.read(max_bytes)
                     if not data:
                         break
+                    bin_pages.append(data)
                     hex_pages.append(data.hex())
                 return_dict["data"] = hex_pages
+                return_dict["bin_data"] = bin_pages
                 return return_dict
         except FileNotFoundError:
             return_dict["status"] = False
@@ -40,24 +50,32 @@ class FlashLoad(PamirSerial):
             return_dict["msg"] = f"Error: {str(e)}"
         return return_dict
 
-    def load_bitstream_file(self, file_name: str, max_size_bytes=MAX_SIZE_BYTES):
+    def load_bitstream_file(self, file_name: str, fpga_type="XCKU040") -> dict:
+        if fpga_type not in FPGA_BITSTREAM_IDCODE:
+            return {
+                "status": False,
+                "msg": f"Unsupported FPGA type: {fpga_type}. Supported types: {', '.join(FPGA_BITSTREAM_IDCODE.keys())}",
+            }
         # check if extension provided in the file name
-        if '.' not in file_name:
+        if "." not in file_name:
             return {"status": False, "msg": "No file extension provided."}
-        # check if correct extensions
-        allowed_exts = [".bit", ".bin" , ".txt"]
         _, ext = os.path.splitext(file_name)
-        if ext.lower() not in allowed_exts:
-            return {"status": False, "msg": f"Unsupported file extension: {ext}. Allowed extensions: {', '.join(allowed_exts)}"}
+        # check if correct extensions
+        if ext.lower() not in ALLOW_EXTS:
+            return {
+                "status": False,
+                "msg": f"Unsupported file extension: {ext}. Allowed extensions: {', '.join(ALLOW_EXTS)}",
+            }
 
         if not os.path.isfile(file_name):
             return {"status": False, "msg": "Error: File not found."}
 
         # check size of the file
         file_size = os.path.getsize(file_name)
-        if file_size > max_size_bytes:
-            return {"status": False,
-                "msg": f"Error: File too large ({file_size} bytes). Max allowed is {max_size_bytes} bytes."
+        if file_size > IMAGE_MAX_SIZE_BYTES:
+            return {
+                "status": False,
+                "msg": f"Error: File too large ({file_size} bytes). Max allowed is {IMAGE_MAX_SIZE_BYTES} bytes.",
             }
         print(f"Size of the file：{file_size} bytes")
 
@@ -66,15 +84,27 @@ class FlashLoad(PamirSerial):
             return return_dict
         else:
             # check syncronisation code "AA995566" exists, etc.,
-            #
-            # with open(file_name, 'rb') as f:
-            #     header = f.read(4).hex()
-            #     if header.lower() != "aa995566":
-            #         return {"status": False, "msg": f"Missing synchronisation code AA995566. First 4 bytes:{header.lower()}"}
-            self.bitstream = return_dict["data"]
+            whole_file_string = "".join(return_dict["data"])
+            if whole_file_string.lower().find(FPGA_BITSTREAM_SYNC_WORD.lower()) == -1:
+                return {
+                    "status": False,
+                    "msg": f"Missing synchronisation code {FPGA_BITSTREAM_SYNC_WORD}.",
+                }
+
+            if (
+                whole_file_string.lower().find(FPGA_BITSTREAM_IDCODE[fpga_type].lower())
+                == -1
+            ):
+                return {
+                    "status": False,
+                    "msg": f"Missing synchronisation code {FPGA_BITSTREAM_SYNC_WORD}.",
+                }
+            self.bitstream: list[bytes] = return_dict["bin_data"]
             return return_dict
 
-    def write_to_flash(self, image_type: Literal["golden","operation"]="operation"):
+    def write_image_to_flash(
+        self, image_type: Literal["golden", "operation"] = "operation"
+    ):
         return_dict = {"status": True, "msg": None}
         if not hasattr(self, "bitstream"):
             return_dict["status"] = False
@@ -83,11 +113,11 @@ class FlashLoad(PamirSerial):
 
         # check image_type
         if image_type == "golden":
-            base_address = FLASH_BASE_GOLDEN_BYTES
-            max_address = FLASH_MAX_GOLDEN_BYTES
+            base_address = FLASH_ADDRBASE_GOLDEN
+            max_address = FLASH_ADDRBASE_GOLDEN + IMAGE_MAX_SIZE_BYTES - 1
         elif image_type == "operation":
-            base_address = FLASH_BASE_OPERATION_BYTES
-            max_address = FLASH_MAX_OPERATION_BYTES
+            base_address = FLASH_ADDRBASE_OPERATION
+            max_address = FLASH_ADDRBASE_OPERATION + IMAGE_MAX_SIZE_BYTES - 1
         else:
             return_dict["status"] = False
             return_dict["msg"] = "Invalid image_type. Use 'golden' or 'operation'."
@@ -95,7 +125,7 @@ class FlashLoad(PamirSerial):
 
         # Erase loop
         print("Erasing sectors...")
-        self.flash_write_enable()
+
         for erase_addr in range(base_address, max_address, FLASH_SECTOR_SIZE):
             print(f"Erasing sector at 0x{erase_addr:08X}")
             try:
@@ -115,28 +145,24 @@ class FlashLoad(PamirSerial):
                 return_dict["msg"] = f"Erase error at 0x{erase_addr:08X}"
                 return return_dict
 
-        self.flash_write_disable()
-
-
         # Write loop
         print("Writing data pages...")
         write_addr = base_address
-        self.flash_write_enable()
-        for idx, hex_page in enumerate(self.bitstream):
+        for idx, bin_page in enumerate(self.bitstream):
             if write_addr >= max_address:
                 print("Warning: Write exceeds assigned flash region.")
                 break
 
-            # hex to str
-            page_bytes = bytes.fromhex(hex_page)
-            page = list(page_bytes)
+            bin_page_list = list(bin_page)
 
-            if len(page) < FLASH_PAGE_SIZE:
-                page += [0xFF] * (FLASH_PAGE_SIZE - len(page))  # Pad incomplete page
+            if len(bin_page_list) < FLASH_PAGE_SIZE:
+                bin_page_list += [0xFF] * (
+                    FLASH_PAGE_SIZE - len(bin_page_list)
+                )  # Pad incomplete page
 
             print(f"Writing page {idx + 1}/{len(self.bitstream)} at 0x{write_addr:08X}")
             try:
-                self.flash_write(write_addr, page)
+                self.flash_write(write_addr, bin_page_list)
                 start = time.time()
                 while self.flash_read_status() & 0x01:  # check flash status
                     if time.time() - start > FLASH_WIP_TIMEOUT_SEC:
@@ -152,34 +178,39 @@ class FlashLoad(PamirSerial):
                 return_dict["msg"] = f"Write error at 0x{write_addr:08X}"
                 return return_dict
 
-
             write_addr += FLASH_PAGE_SIZE  # Move to next 256-byte page
 
         self.flash_write_disable()
         return_dict["msg"] = "Successfully written to flash."
         return return_dict
 
-        
-    def read_from_flash(self, image_type: Literal["golden","operation"], save_to_file: bool = True):
-
+    def read_image_from_flash(
+        self, image_type: Literal["golden", "operation"], save_to_file: bool = False
+    ):
         if image_type == "golden":
-            base_address = FLASH_BASE_GOLDEN_BYTES
-            max_address = FLASH_MAX_GOLDEN_BYTES
+            base_address = FLASH_ADDRBASE_GOLDEN
+            max_address = FLASH_ADDRBASE_GOLDEN + IMAGE_MAX_SIZE_BYTES - 1
         elif image_type == "operation":
-            base_address = FLASH_BASE_OPERATION_BYTES
-            max_address = FLASH_MAX_OPERATION_BYTES
+            base_address = FLASH_ADDRBASE_OPERATION
+            max_address = FLASH_ADDRBASE_OPERATION + IMAGE_MAX_SIZE_BYTES - 1
         else:
-            return {"status": False, "msg": "Invalid image_type. Use 'golden' or 'operation'."}
+            return {
+                "status": False,
+                "msg": "Invalid image_type. Use 'golden' or 'operation'.",
+            }
         print("Reading flash pages...")
 
         read_addr = base_address
-        pages = []
+        list_byte: list[int] = []
 
         while read_addr < max_address:
             try:
                 page = self.flash_read(read_addr)
-                pages.append(page)
-                print(f"Read 256B page from 0x{read_addr:08X}, {page}") # print the address and data
+                list_byte.extend(page)
+                str_hex_page = "".join(f"{b:02x}" for b in page)
+                print(
+                    f"Read 256B page from 0x{read_addr:08X}, 0x{str_hex_page}"
+                )  # print the address and data
             except Exception as e:
                 print(f" Read failed at 0x{read_addr:08X}: {e}")
                 return {"status": False, "msg": f"Read error at {hex(read_addr)}"}
@@ -190,37 +221,20 @@ class FlashLoad(PamirSerial):
         if save_to_file:
             file_path = f"{image_type}_read_back.txt"
             try:
-                with open(file_path, 'w') as f:
-                    for page in pages:
-                        hex_str = ''.join(f"{b:02x}" for b in page)
+                with open(file_path, "w") as f:
+                    for page in list_byte:
+                        hex_str = "".join(f"{b:02x}" for b in page)
                         f.write(hex_str + "\n")
                 print(f"Flash content saved to: {file_path}")
             except Exception as e:
                 return {"status": False, "msg": f"Failed to save file: {str(e)}"}
-            return {"status": True, "msg": f"Read {len(pages)} pages and saved to {file_path}", "data": pages}
-        return {"status": True, "msg": f"Read {len(pages)}", "data": pages}
-
-    def read_flash_id(self):
-        """
-        Returns {"status": bool, "hex": str, "msg": str}
-        """
-        try:
-            raw_id = self.flash_read_id()
-        except Exception as exc:
-            return {"status": False, "msg": f"Read-ID command failed: {exc}"}
-
-        jedec_id = raw_id & 0xFFFFFF
-        hex_id   = f"{jedec_id:06x}"
-
-        if hex_id != EXPECTED_FLASH_ID:
-            return {"status": False,
-                    "hex": hex_id,
-                    "msg": f"Wrong board detected (ID {hex_id} ≠ {EXPECTED_FLASH_ID})"}
-
-        return {"status": True,
-                "hex": hex_id,
-                "msg": f"Flash ID OK: {hex_id}"}
-
-
-
-
+            return {
+                "status": True,
+                "msg": f"Read {len(list_byte)} pages and saved to {file_path}",
+                "data": list_byte,
+            }
+        return {
+            "status": True,
+            "msg": f"Read {len(list_byte)} bytes",
+            "data": list_byte,
+        }
--- test_main.py
+++ test_main.py
@@ -2,26 +2,20 @@ import sys
 
 import serial
 from flash_load import FlashLoad
-PORT  = "/dev/ttyS0"
-BAUD  = 230400
+
+PORT = "/dev/ttyS0"
+BAUD = 230400
 
 try:
     serial_port = serial.Serial(PORT, BAUD, timeout=1)
 except serial.SerialException as e:
-    err = {
-        "status": False,
-        "msg": f"Could not open port: {e}"
-    }
+    err = {"status": False, "msg": f"Could not open port: {e}"}
     print(err)
     sys.exit(1)
 fl = FlashLoad(serialport=serial_port, timeout=1)
 load_result = fl.load_bitstream_file("app.bin")
 print(f"Result:{load_result['status']}, {load_result['msg']}")
-res = fl.read_flash_id()
-if not res["status"]:
-    print(res["msg"])
-    sys.exit(1)
-wr = fl.write_to_flash("operation")
+wr = fl.write_image_to_flash("operation")
 print(f"Result:{wr['status']}, {wr['msg']}")
-rd = fl.read_from_flash("operation", True)
+rd = fl.read_image_from_flash("operation")
 print(f"Result:{rd['status']}, {rd['msg']}")
